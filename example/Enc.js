ea0bdd07ba379a7a4d3f938289d1f3affb0468be
Hongyerim0 <dpfla927@naver.com>
2023-09-15 13:31:02 +0900

    (MYSQL) dotenv 설정 및 커넥션 설정 완료

--- example/Enc.js
+++ example/Enc.js
@@ -10,21 +10,24 @@ const sha256Enc = (plainText, key) => {
 };
 
 const AESEncrypt = (plainTxt) => {
-    const algorithm = "aes-256-cbc";
-    const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
-    const iv = "dwUgwmwOndnJOmOX";
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encrypted = cipher.update(plainTxt, "utf8", "base64");
-    encrypted += cipher.final("base64");
-    console.log(encrypted);
-    return encrypted;
-  };
-  const AESDecrypt = (plainTxt) => {
-    const algorithm = "aes-256-cbc";
-    const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
-    const iv = "dwUgwmwOndnJOmOX";
-    const cipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = cipher.update(plainTxt, "base64", "utf8");
-    decrypted += cipher.final("utf8");
-    console.log(decrypted);
-  };
+  const algorithm = "aes-256-cbc";
+  const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
+  const iv = "dwUgwmwOndnJOmOX";
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(plainTxt, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  console.log(encrypted);
+  return encrypted;
+};
+
+const AESDecrypt = (plainTxt) => {
+  const algorithm = "aes-256-cbc";
+  const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
+  const iv = "dwUgwmwOndnJOmOX";
+  const cipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = cipher.update(plainTxt, "base64", "utf8");
+  decrypted += cipher.final("utf-8");
+  console.log(decrypted);
+};
+AESEncrypt("!Kwic123테스트");
+AESDecrypt("b3Bu7QBZ0q96+f2GY2NQmaT1XnZElI1+wRdSHBOY01s=");
