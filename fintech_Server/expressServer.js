8bb1f635899ec21ac09e9c71e2051c4f429bf928
Hongyerim0 <dpfla927@naver.com>
2023-09-15 14:34:46 +0900

    (서버) 로그인 기능

--- fintech_Server/expressServer.js
+++ fintech_Server/expressServer.js
@@ -1,8 +1,18 @@
-const espress =require("express");
-const dotenv= require("dotenv");
+const express = require("express");
+const dotenv = require("dotenv");
+const mysql = require("mysql2");
 const app = express();
 
 dotenv.config();
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_ACCOUNT,
+  password: process.env.DB_PASSWORD,
+  database: "fintech",
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req,res) {
     res.send("Hello world!");
@@ -10,7 +20,20 @@ app.get("/", function (req,res) {
 
 app.post("/",(req, res)=>{
     console.log(req);
+    res.send("Hello");
+});
 
+app.post("/login",(req, res)=>{
+    const{ userAccount, password}=req.body;
+    const sql =
+    "SELECT user_id, user_account, user_password FROM fintech.user WHERE user_account = ?";
+    Connection.query(sql,[userAccount],(err, result)=>{
+        if (err) throw err;
+        console.log(result);
+        if(password===result[0].user_password){
+            res.json("로그인 성공");
+        }
+    });
 });
 
 app.listen(process.env.PORT)
